f43e38bf91c79170cda7254ab0a4011838a41865
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-09 00:24:53 +0700

    Create Anggota.java

---
+++ model/Anggota.java
@@ -0,0 +1,35 @@
+public class Anggota {
+    private String nobp;
+    private String nama;
+    private String alamat;
+
+    public Anggota() {
+    }
+
+    public Anggota(String nobp, String nama, String alamat) {
+        this.nobp = nobp;
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public String getNobp() {
+        return nobp;
+    }
+    public void setNobp(String nobp) {
+        this.nobp = nobp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+     public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
